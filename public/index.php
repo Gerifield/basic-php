51f17d5576c1c0e215bd7c158d359004f7f0e30a
gerifield <radics.gergely@ustream.tv>
2015-09-23 11:14:12 +0200

    Added controller call

--- public/index.php
+++ public/index.php
@@ -2,3 +2,30 @@
 
 require __DIR__.'/../vendor/autoload.php';
 
+
+$router = Router::getInstance()->getRouter();
+$match  = $router->match();
+
+ob_start();
+session_start();
+
+if ($match) {
+	if (strstr($match['target'], '@')) {
+		$route = explode('@', $match['target']);
+
+		//Create and call controller
+		$controller = 'Controller\\'.$route[0];
+		$controller = new $controller($router);
+		$method     = $route[1];
+		call_user_func_array( array($controller, $method), $match['params'] );
+
+	} else if (is_callable($match['target'])) {
+		call_user_func_array( $match['target'], $match['params'] );
+	} else {
+		header( $_SERVER["SERVER_PROTOCOL"] . ' 500 Internal server error');
+	}
+} else {
+	// no route was matched
+	header( $_SERVER["SERVER_PROTOCOL"] . ' 404 Not Found');
+}
+ob_end_flush();
