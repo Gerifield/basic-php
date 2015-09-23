124c3bb6fdb0c6879b44870751c0c3fe0eb88d56
gerifield <radics.gergely@ustream.tv>
2015-09-23 10:35:00 +0200

    Added test

--- test/UnitTest/Gerifield/RouterTest.php
+++ test/UnitTest/Gerifield/RouterTest.php
@@ -5,7 +5,39 @@
 
 namespace UnitTest\Gerifield;
 
+use Gerifield\Router;
+
 class RouterTest extends \PHPUnit_Framework_TestCase
 {
 
+	/**
+	 * @var \PHPUnit_Framework_MockObject_MockObject
+	 */
+	private $altoRouter;
+
+	/**
+	 * @var Router
+	 */
+	private $object;
+
+	protected function setUp()
+	{
+		parent::setUp();
+
+		$this->altoRouter = $this->getMock('\AltoRouter');
+
+		$this->object = new Router(
+			$this->altoRouter
+		);
+	}
+
+	public function testMatch()
+	{
+		$this->altoRouter->expects($this->once())
+			->method('match')
+			->with('testUrl', 'testMethod')
+			->willReturn('success');
+
+		$this->assertEquals('success', $this->object->match('testUrl', 'testMethod'));
+	}
 }
